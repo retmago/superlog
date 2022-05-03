3a38de624aff1e71a1d12f868145507dde7e8147
jose <jsilva2018.95@gmail.com>
2022-05-03 19:57:15 -0300

    se agrego informacion

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='superlog',
+    packages=['superlog'],
+    version='0.1',
+    license='LGPL v3',  # La licencia que tenga tu paquete
+    description='Libreria para mostrar mayor cantidad de informacion en los logs de manera facil',
+    author='jsilva95',
+    author_email='jsilva2018.95@gmail.com',
+    url='https://github.com/retmago/superlog.git',  # Usa la URL del repositorio de GitHub
+    download_url='https://github.com/RDCH106/parallel_foreach_submodule/archive/v0.1.tar.gz',
+    # Te lo explico a continuación
+    keywords='test example develop',  # Palabras que definan tu paquete
+    classifiers=['Programming Language :: Python',
+                 # Clasificadores de compatibilidad con versiones de Python para tu paquete
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7'],
+)
